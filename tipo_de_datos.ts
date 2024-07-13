c45f007876a4b66f75894608e273d5d6f3a7d0aa
zolynux <robin-0507@outlook.com>
2024-07-12 20:26:39 -0500

    tema: tipo de datos (incompleto)

---
+++ tipo_de_datos.ts
@@ -0,0 +1,20 @@
+// --------------------- String ---------------------
+
+// Comillas dobles:
+const str1: string = "Hola, soy una cadena con comillas dobles."
+// Comillas simples:
+const str2: string = 'Hola, soy una cadena con comillas dobles.'
+
+// Template literals (backticks)
+const nombre: string = 'Juan'
+const edad: number = 30
+
+const str3: string = `Hola, mi nombre es ${nombre} y tengo ${edad} años.`
+
+// --------------------- Numeros ---------------------
+
+// Números enteros:
+const num1: number = 20
+
+// Número exponencial:
+
